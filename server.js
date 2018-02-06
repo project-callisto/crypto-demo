e619dc06b2439f4917c6fa2a2add0b0a8795be1b
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-06 14:57:10 -0800

    serve from angular's built folder

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var server = app.listen(8080, function() {
     console.log('Listening on port %d', server.address().port)
 });
 
-
 app.get('/', function(req,res) {
-    res.sendFile((path.join(__dirname + '/index.html')));
+    res.sendFile((path.join(__dirname, 'dist/index.html')));
 });
