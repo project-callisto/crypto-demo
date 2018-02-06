4727ecacb3759b1e1db99c924acb5d93d3e895fb
Lucy Qin <lucyqin@Lucys-MacBook-Air.local>
2018-02-06 09:54:13 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+
+var server = app.listen(8080, function() {
+    console.log('Listening on port %d', server.address().port)
+});
+
+
+app.get('/', function(req,res) {
+    res.sendFile((path.join(__dirname + '/index.html')));
+});
