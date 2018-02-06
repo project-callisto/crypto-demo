8daf568336bb0c298120db29b341509c500d76a1
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-06 15:54:55 -0800

    eventual DRY

--- protractor/spec.js
+++ protractor/spec.js
@@ -1,6 +1,11 @@
 describe('Valkyrie Demo', function() {
-  it('should have a title', function() {
+
+  beforeEach(function() {
     browser.get('/');
+  });
+
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Valkyrie Demo');
   });
+
 });
