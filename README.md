5f33681c66e8b90c63272958f8728d2a135b3f0e
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-06 22:39:57 -0800

    dev VS prod setup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Valkyrie Demo
+
+## running the dev server
+
+```
+npm start
+```
+
+## running a (mock, local) production server
+
+```
+npm run heroku-postbuild
+heroku local
+```
--- web-server/webpack.config.js
+++ web-server/webpack.config.js
@@ -1,64 +1,72 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var NpmInstallPlugin = require('npm-install-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const {getIfUtils} = require('webpack-config-utils')
 
-module.exports = {
-  entry: {
-    vendor: [
-      path.join(__dirname, '/../client/polyfills.ts'),
-      path.join(__dirname, '/../client/vendor.ts'),
-    ],
-    app: path.join(__dirname, '/../client/main.ts'),
-  },
-  output: {
-    filename: "[name].js",
-    path: path.join(__dirname, '/../dist')
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  watch: true,
-  devtool: 'cheap-source-map',
-  module: {
-    rules: [
-    {
-      test: /\.ts$/,
-      loaders: [
+module.exports = function(env, argv) {
+  const {ifDev} = getIfUtils(
+    Object.is(process.env.NODE_ENV, undefined) ? "dev" : process.env.NODE_ENV
+  )
+
+  return {
+    entry: {
+      vendor: [
+        path.join(__dirname, '/../client/polyfills.ts'),
+        path.join(__dirname, '/../client/vendor.ts'),
+      ],
+      app: path.join(__dirname, '/../client/main.ts'),
+    },
+    output: {
+      filename: "[name].js",
+      path: path.join(__dirname, '/../dist')
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    watch: ifDev(),
+    devtool: 'cheap-source-map',
+    module: {
+      rules: [
       {
-        loader: 'awesome-typescript-loader',
-        options: { configFileName: path.join(__dirname, '/../tsconfig.json') }
-      } , 'angular2-template-loader'
+        test: /\.ts$/,
+        loaders: [
+        {
+          loader: 'awesome-typescript-loader',
+          options: { configFileName: path.join(__dirname, '/../tsconfig.json') }
+        } , 'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: path.join(__dirname, '/../client/app'),
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.css$/,
+        include: path.join(__dirname, '/../client/app'),
+        loader: 'raw-loader'
+      }
       ]
     },
-    {
-      test: /\.html$/,
-      loader: 'html-loader'
-    },
-    {
-      test: /\.css$/,
-      exclude: path.join(__dirname, '/../client/app'),
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: 'css-loader'
-      })
-    },
-    {
-      test: /\.css$/,
-      include: path.join(__dirname, '/../client/app'),
-      loader: 'raw-loader'
-    }
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: Infinity,
+      }),
+      new HtmlWebpackPlugin({
+        template: 'client/index.html',
+      }),
+      new NpmInstallPlugin()
     ]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-    }),
-    new HtmlWebpackPlugin({
-      template: 'client/index.html',
-    }),
-    new NpmInstallPlugin()
-  ]
-};
+  }
+
+}
