1d15877f27cd5e634cdd7cab2fbeb8a8eb659342
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-07 17:43:35 -0800

    resources and imports refactor

--- client/app/first-step.component.ts
+++ client/app/first-step.component.ts
@@ -4,6 +4,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'content-root',
   templateUrl: './first-step.component.html',
+  styleUrls: [
+    './styles/base.scss',
+    './styles/step.scss',
+  ],
 })
 export class FirstStepComponent {
 }
