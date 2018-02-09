27045fabc9a43f78024f419ffdee0df98809b1da
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-09 11:23:19 -0800

    linter changes

--- client/app/crypto.service.ts
+++ client/app/crypto.service.ts
@@ -4,22 +4,23 @@ import * as $ from "jquery";
 
 // all of crypto functions
 export class CryptoService {
-    public run() {
-        const record = {
-            perpName: "harvey weinstein",
-            perpEmail: "harvey@weinstein.com",
-        };
+  public run() {
+    const record = {
+      perpName: "harvey weinstein",
+      perpEmail: "harvey@weinstein.com",
+    };
 
-        for (let i = 0; i < 2; i++) {
-            $.post("http://localhost:8080/postPerpId", {
-                pid: "https://www.facebook.com/weinsteinharvey/?ref=br_rs",
-              }, function(data, status) {
-                  // output from PRF
-                const rid = parseInt(data.rid, 16);
-                console.log("original rid: ", rid);
-
-            });
-        }
+    for (let i = 0; i < 2; i++) {
+      $.post(
+        "http://localhost:8080/postPerpId",
+        {pid: "https://www.facebook.com/weinsteinharvey/?ref=br_rs"},
+        (data, status) => {
+          // output from PRF
+          const rid = parseInt(data.rid, 16);
+          console.log("original rid: ", rid);
+        },
+      );
     }
+  }
 }
 
