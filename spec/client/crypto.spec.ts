20a6b122104b386ca4e5de14506db5a7f47c9723
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2018-02-12 19:24:34 +0000

    Added tests for crypto

---
+++ spec/client/crypto.spec.ts
@@ -0,0 +1,44 @@
+import { CryptoService } from "../../client/app/crypto.service";
+
+interface EncryptedData {
+    x?: number;
+    y?: number;
+    encryptedRecordKey?: object;
+    encryptedRecord?: object;
+    hashedPerpId?: string;
+    rid?: number;
+}
+
+describe("Crypto services tests", function() {
+    let crypto = new CryptoService();
+
+    it("has an encryptData method", function() {
+	expect(crypto.encryptData).toBeDefined();
+    });
+
+    it("has an decryptData method", function() {
+	expect(crypto.decryptData).toBeDefined();
+    });
+
+    it("can encrypt data", function() {
+	const data: EncryptedData = crypto.encryptData('a');
+	expect(data.encryptedRecordKey).toBeTruthy();
+    });
+
+    it("has an RID", function() {
+	const data: EncryptedData = crypto.encryptData('a');
+	expect(data.rid).toBeTruthy();
+    });
+
+    it("returns RID for perpIDs starting with A-Z", function() {
+	var perpID: number = 65;
+	var maxPerpID: number = 90;
+	
+	while (perpID <= maxPerpID)
+	{
+	    const data: EncryptedData = crypto.encryptData(String.fromCharCode(perpID));
+	    expect(data.rid).toBeTruthy('Using perpID "' + String.fromCharCode(perpID) + '"');
+	    perpID++;
+	}
+    });
+});
