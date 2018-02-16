b3b99c157eb89a47774281b92c5f265cccdc7e7b
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-16 13:52:12 -0800

    add steps 3 - 6

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { FirstStepComponent } from "./first-step.component";
 import { HeaderComponent } from "./header.component";
-import { SecondStepComponent } from "./second-step.component";
 import { StepComponent } from "./step.component";
+import { FirstStepComponent } from "./first-step.component";
+import { SecondStepComponent } from "./second-step.component";
+import { ThirdStepComponent } from "./third-step.component";
+import { FourthStepComponent } from "./fourth-step.component";
+import { FifthStepComponent } from "./fifth-step.component";
+import { SixthStepComponent } from "./sixth-step.component";
 
 @NgModule({
   declarations: [
@@ -14,6 +18,10 @@ import { StepComponent } from "./step.component";
     StepComponent,
     FirstStepComponent,
     SecondStepComponent,
+    ThirdStepComponent,
+    FourthStepComponent,
+    FifthStepComponent,
+    SixthStepComponent,
   ],
   imports: [
     BrowserModule,
--- client/app/second-step.component.ts
+++ client/app/second-step.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewContainerRef } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { EncryptedData } from "./services/crypto.service";
 
 @Component({
