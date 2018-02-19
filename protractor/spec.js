4ae6c6d54d92770b212cfcbcc1b4bcbf635e260b
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-18 22:43:09 -0800

    step 2 spec

--- protractor/spec.js
+++ protractor/spec.js
@@ -3,35 +3,44 @@
 // jasmine docs https://jasmine.github.io/api/3.0/matchers.html
 // // jasmine defines methods like toEqual, expect, toContain
 
-describe('Valkyrie Demo', function() {
-  const FirstSection = element(by.css('first-step'));
+describe('Valkyrie Demo', function () {
+  const FirstStep = element(by.css('first-step section'));
+  const SecondStep = element(by.css('second-step section'));
+  const ThirdStep = element(by.css('third-step section'));
+  const FourthStep = element(by.css('fourth-step section'));
+  const FifthStep = element(by.css('fifth-step section'));
+  const SixthStep = element(by.css('sixth-step section'));
   const PerpNameInput = element(by.css('.perp-name-form [type="text"]'));
   const PerpNameSubmit = element(by.css('.perp-name-form [type="submit"]'));
   const RIDDisplay = element(by.css('.rid-display'));
 
-  beforeEach(function() {
+  const genericPerpInput = function () {
+    PerpNameInput.sendKeys('facebook.com/callistoorg');
+    PerpNameSubmit.click();
+  }
+
+  beforeEach(function () {
     browser.get('/');
   });
 
-  it('should have a title', function() {
+  it('should have a title', function () {
     expect(browser.getTitle()).toEqual('Valkyrie Demo');
   });
 
-  it('should start with a perp name displayed', function() {
-    expect(FirstSection.getText()).toContain('PERP NAME');
+  it('should start with a perp name displayed', function () {
+    expect(FirstStep.getText()).toContain('PERP NAME');
   });
 
-  it('starts with no RID rendered', function() {
+  it('starts with no RID rendered', function () {
     expect(RIDDisplay.getText()).toContain('[[ RID ]]');
   });
 
-  it('renders a RID after perp name input', function() {
-    PerpNameInput.sendKeys('facebook.com/callistoorg');
-    PerpNameSubmit.click();
+  it('renders a RID after perp name input', function () {
+    genericPerpInput();
     expect(RIDDisplay.getText()).not.toContain('[[ RID ]]');
   });
 
-  it('renders a RID for perp names starting with a', function() {
+  it('renders a RID for perp names starting with a', function () {
     PerpNameInput.sendKeys('apple');
     PerpNameSubmit.click();
     expect(RIDDisplay.getText()).not.toContain('NaN');
@@ -42,5 +51,11 @@ describe('Valkyrie Demo', function() {
   //   PerpNameSubmit.click();
   //   expect(RIDDisplay.getText()).not.toContain('NaN');
   // });
+
+  it('advances to step 2', function () {
+    expect(SecondStep.isPresent()).toBeFalsy();
+    genericPerpInput();
+    expect(SecondStep.isPresent()).toBeTruthy();
+  });
 
 });
