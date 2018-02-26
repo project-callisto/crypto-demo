35b788bce936167c1d4863febeeb21c33f17c63c
Lynn Cyrin <firemagelynn@gmail.com>
2018-02-25 16:12:05 -0800

    remove some fluff

--- unit-tests/karma.conf.js
+++ unit-tests/karma.conf.js
@@ -21,22 +21,14 @@ module.exports = function (config) {
         allowJs: true,
       }
     },
-    // reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
       'spec',
       "karma-typescript"
     ],
     port: 9876,
     colors: true,
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
     browsers: ['Chrome'],
-    // [ dev mode ]
-    autoWatch: true,
-    singleRun: false,
-    // [ ci mode ]
-    // autoWatch: false,
-    // singleRun: true,
     concurrency: Infinity
   })
 }
