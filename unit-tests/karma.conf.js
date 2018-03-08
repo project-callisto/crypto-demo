97a1a16730bfaf277e18ab9a0af434e5bfbe515d
Lynn Cyrin <firemagelynn@gmail.com>
2018-03-08 09:43:53 -0800

    angular cli test config

--- unit-tests/karma.conf.js
+++ unit-tests/karma.conf.js
@@ -6,41 +6,26 @@ module.exports = function (config) {
     basePath: "./../",
     frameworks: [
       "jasmine",
-      "karma-typescript",
       '@angular/cli',
     ],
     plugins: [
       require('karma-jasmine'),
-      require('karma-typescript'),
       require('@angular/cli/plugins/karma'),
+      require('karma-chrome-launcher'),
+      require("karma-spec-reporter"),
     ],
     files: [
       "client/app/services/crypto.service.ts",
       "unit-tests/client/*[sS]pec.ts",
     ],
-    preprocessors: {
-      "**/*.ts": "karma-typescript",
-    },
     angularCli: {
       environment: 'dev'
     },
-    karmaTypescriptConfig: {
-      tsconfig: "unit-tests/tsconfig.json",
-      compilerOptions: {
-        allowJs: true,
-      },
-      bundlerOptions: {
-        entrypoints: /\.[sS]pec\.ts$/,
-        sourceMap: true,
-        validateSyntax: true,
-      },
-      coverageOptions: {
-        instrumentation: false,
-      }
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     reporters: [
       "spec",
-      "karma-typescript",
     ],
     port: 9876,
     colors: true,
