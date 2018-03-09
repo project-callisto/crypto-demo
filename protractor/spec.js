8b49960fa0df700392872cffeb2662dd997d41ec
Lynn Cyrin <firemagelynn@gmail.com>
2018-03-09 14:44:00 -0800

    article 3 test

--- protractor/spec.js
+++ protractor/spec.js
@@ -3,17 +3,16 @@
 // jasmine docs https://jasmine.github.io/api/3.0/matchers.html
 // // jasmine defines methods like toEqual, expect, toContain
 
-describe('Valkyrie Demo', () => {
-
+describe("Valkyrie Demo", () => {
   beforeEach(() => {
-    browser.get('/');
+    browser.get("/");
   });
 
-  it('should have a title', () => {
-    expect(browser.getTitle()).toEqual('Valkyrie Demo');
+  it("should have a title", () => {
+    expect(browser.getTitle()).toEqual("Valkyrie Demo");
   });
 
-  it('advances to step 1', () => {
+  it("advances to article 2", () => {
     // there's only 1 article visible on page start
     expect(article(1).isPresent()).toBeTruthy();
     expect(article(2).isPresent()).toBeFalsy();
@@ -21,12 +20,29 @@ describe('Valkyrie Demo', () => {
     article(1).next();
     // and now a second article is visible
     expect(article(2).isPresent()).toBeTruthy();
-  })
+  });
+
+  it("advances to article 3", () => {
+    expect(article(3).isPresent()).toBeFalsy();
+    article(1).next();
+    article(2).next();
+    expect(article(3).isPresent()).toBeTruthy();
+  });
 
   function article(index) {
     const element = $(`step-root *:nth-child(${index}) article`);
-    element.next = () => { element.$('.advance-button').click(); }
-    return element
+    if (index === 2) {
+      element.next = () => {
+        element.$("#userInput").sendKeys('user name');
+        element.$("#perpInput").sendKeys('perp id');
+        element.$(".advance-button").click();
+      }
+    } else {
+      element.next = () => {
+        element.$(".advance-button").click();
+      }
+    }
+    return element;
   }
 
 });
