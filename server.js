e5df2c927c59255540eeae4cf3a253803693a41e
Lynn Cyrin <firemagelynn@gmail.com>
2018-03-08 18:15:05 -0800

    static file serving

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var server = app.listen(PORT, () => {
   console.log('Listening on port %d', server.address().port)
 });
 
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '/dist')));
+
+// Catch all other routes and return the index file
+// IMPORTANT: this route needs to come last
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
