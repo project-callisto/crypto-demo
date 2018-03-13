28fc4eac4377e8e4540cf7b5f4538672084ef619
Lynn Cyrin <firemagelynn@gmail.com>
2018-03-12 18:28:25 -0700

    add skip to main

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -3,10 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   template: `
+    <a href="#first-step" class="skip-to-main sr-only-focusable">Skip to main content</a>
     <header-root class="main-content"></header-root>
     <step-root class="main-content"></step-root>
     <footer-root class="main-content"></footer-root>
   `,
+  styleUrls: [
+    "./styles/base.scss",
+  ],
 })
 export class AppComponent {
 }
