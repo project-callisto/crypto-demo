241791e8cf04f2fe0b5848a38de1f53b7fb3c25d
Lynn Cyrin <firemagelynn@gmail.com>
2018-03-26 16:35:38 -0700

    checkin

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -9,34 +8,12 @@ var server = app.listen(PORT, () => {
   console.log('Listening on port %d', server.address().port)
 });
 
-app.use(cookieParser());
-
-// app.use('/.application', (req, res, next) => {
-//   res.cookie('appType', 'main');
-//   res.redirect('/');
-//   next();
-// });
-
-// app.use('/.storybook', (req, res, next) => {
-//   res.cookie('appType', 'storybook');
-//   res.redirect('/');
-//   next();
-// });
-
-app.use((req, res, next) => {
-  if (req.cookies.appType === 'storybook') {
-    express.static(path.join(__dirname, '/storybook-dist'));
-  } else {
-    express.static(path.join(__dirname, '/dist'));
-  }
-  next();
+app.use('/.storybook', express.static(path.join(__dirname, '/storybook-dist/')));
+app.get('/.storybook', (req, res) => {
+  res.sendFile(path.join(__dirname, '/storybook-dist/index.html'));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-  // if (req.cookies.appType === 'storybook') {
-  //   return res.sendFile(path.join(__dirname, '/storybook-dist/index.html'));
-  // } else {
-  //   return res.sendFile(path.join(__dirname, '/dist/index.html'));
-  // }
-});
+// app.use(express.static(path.join(__dirname, '/dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/dist/index.html'));
+// });
