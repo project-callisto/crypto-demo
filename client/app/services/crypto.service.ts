8430b402afffd4597cedeacd46087ca4049db1b8
lucyq <lucyzqin@gmail.com>
2018-03-27 10:18:49 -0400

    more merging

--- client/app/services/crypto.service.ts
+++ client/app/services/crypto.service.ts
@@ -198,11 +198,7 @@ export class CryptoService {
     return {
       decryptedRecords,
       slope,
-<<<<<<< HEAD
       k,
-=======
-      intercept,
->>>>>>> 7a7d831f82d43670ce80764434334823cf632dad
       coords: this.getCoords(),
     };
   }
